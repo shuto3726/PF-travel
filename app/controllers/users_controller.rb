63c16beb1c68f280446e0118c591ad1ec261891b
shuto3726 <sss3726mmm@gmail.com>
2021-06-25 08:34:19 +0000

    finish-authority

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :ensure_correct_user, only: [:edit, :update]
+  
   def index
     @users = User.all
   end
@@ -35,5 +38,12 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:name, :profile_image)
+  end
+  
+  def ensure_correct_user
+    @user = User.find(params[:id])
+    unless @user == current_user
+      redirect_to user_path(current_user)
+    end
   end
 end
